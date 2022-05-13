906e9f4b1f4623a184699f6c33e7948467b3306a
ho3einmolavi <ho3einmolavi00@gmail.com>
2022-05-13 19:32:22 +0430

    second commit

---
+++ components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="elevation-12">
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>Login form</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <form ref="form" @submit.prevent="login()">
+            <v-text-field
+              v-model="email"
+              name="email"
+              label="email"
+              type="text"
+              placeholder="email"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="password"
+              name="password"
+              label="Password"
+              type="password"
+              placeholder="password"
+              required
+            ></v-text-field>
+            <v-btn type="submit" class="mt-4" color="primary" value="log in"
+              >Login</v-btn
+            >
+          </form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      const { email } = this;
+      console.log(email + "logged in");
+    },
+  },
+};
+</script>
---
+++ components/Resevation.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center">
+    <v-expansion-panels inset>
+      <v-expansion-panel
+        v-for="(item,i) in 5"
+        :key="i"
+      >
+        <v-expansion-panel-header>today</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          you book a meeting with Dr.Ja for today
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "reservation",
+};
+</script>
