e8730c9625c55130c73fda0f8140b387fc45406c
ho3einmolavi <ho3einmolavi00@gmail.com>
2022-05-25 10:53:07 +0430

    change photos

--- components/Clinic.vue
+++ components/Clinic.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-card
-    :loading="loading"
-    class="mx-auto my-12"
-    max-width="374"
-  >
+  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
     <template slot="progress">
       <v-progress-linear
         color="deep-purple"
@@ -12,18 +8,12 @@
       ></v-progress-linear>
     </template>
 
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
+    <v-img height="250" src="/img/clinic.jpg"></v-img>
 
-    <v-card-title>Cafe Badilico</v-card-title>
+    <v-card-title>Milan Clinic</v-card-title>
 
     <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0"
-      >
+      <v-row align="center" class="mx-0">
         <v-rating
           :value="4.5"
           color="amber"
@@ -33,16 +23,15 @@
           size="14"
         ></v-rating>
 
-        <div class="grey--text ms-4">
-          4.5 (413)
-        </div>
+        <div class="grey--text ms-4">4.5 (413)</div>
       </v-row>
 
-      <div class="my-4 text-subtitle-1">
-        $ • Italian, Cafe
-      </div>
+      <div class="my-4 text-subtitle-1">Dentists, psychology</div>
 
-      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+      <div>
+        Small plates, salads & sandwiches - an intimate setting with 12 indoor
+        seats plus patio seating.
+      </div>
     </v-card-text>
 
     <v-divider class="mx-4"></v-divider>
@@ -66,11 +55,7 @@
     </v-card-text>
 
     <v-card-actions>
-      <v-btn
-        color="deep-purple lighten-2"
-        text
-        @click="reserve"
-      >
+      <v-btn color="deep-purple lighten-2" text @click="reserve">
         Reserve
       </v-btn>
     </v-card-actions>
@@ -94,5 +79,5 @@ export default {
       }, 2000);
     },
   },
-}
+};
 </script>
